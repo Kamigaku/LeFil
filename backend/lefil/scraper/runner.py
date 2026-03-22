e52d3d6b25e0dde7d64106205a6968dfb87af6fe
Kamigaku <kamigaku@gmail.com>
2026-03-22 11:47:19 +0100

    Added a log for blacklist.

--- backend/lefil/scraper/runner.py
+++ backend/lefil/scraper/runner.py
@@ -97,6 +97,7 @@ class ScraperRunner:
             try:
                 result = active_tasks[i][1].result()
                 if result is None:
+                    logger.info(f"[runner] Adding entry {active_tasks[i][0].link} to the blacklist.")
                     with open(C__DEFAULT_BLACKLIST_FILE, mode="a+", encoding="utf-8") as f:
                         f.write(f"{active_tasks[i][0].link}{os.linesep}")
                     continue
